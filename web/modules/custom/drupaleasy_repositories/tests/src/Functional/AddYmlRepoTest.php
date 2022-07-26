58fab6c29cebdf3b8408995c23b2015d5087f652
Michael Anello <michael@anelloconsulting.com>
2022-07-26 21:24:23 +0200

    End of class 6.

--- web/modules/custom/drupaleasy_repositories/tests/src/Functional/AddYmlRepoTest.php
+++ web/modules/custom/drupaleasy_repositories/tests/src/Functional/AddYmlRepoTest.php
@@ -4,12 +4,17 @@ namespace Drupal\Tests\drupaleasy_repositories\Functional;
 
 use Drupal\Tests\BrowserTestBase;
 
+//use Drupal\Tests\drupaleasy_repositories\Traits\RepositoryContentTypeTrait;
+//use Drupal\field\Entity\FieldStorageConfig;
+//use Drupal\field\Entity\FieldConfig;
+
 /**
  * Test description.
  *
  * @group drupaleasy_repositories
  */
 class AddYmlRepoTest extends BrowserTestBase {
+  //use RepositoryContentTypeTrait;
 
   /**
    * {@inheritdoc}
@@ -19,12 +24,7 @@ class AddYmlRepoTest extends BrowserTestBase {
   /**
    * {@inheritdoc}
    */
-  protected static $modules = [
-    'drupaleasy_repositories',
-    'node',
-    'link',
-    'user',
-  ];
+  protected static $modules = ['drupaleasy_repositories'];
 
   /**
    * {@inheritdoc}
@@ -37,20 +37,62 @@ class AddYmlRepoTest extends BrowserTestBase {
     $config->save();
 
     // Create and login as a Drupal admin user with permission to access
-    // the DrupalEasy Repositories Settings page. This is UID=1.
+    // the DrupalEasy Repositories Settings page. This is UID=2 because UID=1
+    // is created by FunctionalTestSetupTrait. The root user can be accessed
+    // with $this->rootUser.
     $admin_user = $this->drupalCreateUser(['drupaleasy repositories configure']);
     $this->drupalLogin($admin_user);
+
+//    $this->createRepositoryContentType();
+//
+//    FieldStorageConfig::create([
+//      'field_name' => 'field_repository_url',
+//      'type' => 'link',
+//      'entity_type' => 'user',
+//      'cardinality' => -1,
+//    ])->save();
+//    FieldConfig::create([
+//      'field_name' => 'field_repository_url',
+//      'entity_type' => 'user',
+//      'bundle' => 'user',
+//      'label' => 'Repository URL',
+//    ])->save();
+//
+    /** @var \Drupal\Core\Entity\EntityDisplayRepository $entity_display_repository  */
+    $entity_display_repository = \Drupal::service('entity_display.repository');
+    $entity_display_repository->getFormDisplay('user', 'user', 'default')
+      ->setComponent('field_repository_url', ['type' => 'link_default'])
+      ->save();
 
   }
 
   /**
    * Test callback.
    */
-  public function testSomething() {
-    $admin_user = $this->drupalCreateUser(['access administration pages']);
-    $this->drupalLogin($admin_user);
-    $this->drupalGet('admin');
-    $this->assertSession()->elementExists('xpath', '//h1[text() = "Administration"]');
+//  public function testSomething():void {
+//    $another_user = $this->drupalCreateUser(['access administration pages']);
+//    $this->drupalLogin($another_user);
+//    $this->drupalGet('admin');
+//    $this->assertSession()->elementExists('xpath', '//h1[text() = "Administration"]');
+//  }
+
+  /**
+   * Test that the settings page can be reached and works as expected.
+   *
+   * This tests that an admin user can access the settings page, select a
+   * plugin to enable, and submit the page successfully.
+   *
+   * @test
+   */
+  public function testSettingsPage(): void {
+    // Start the browsing session.
+    $session = $this->assertSession();
+
+    // Navigate to the DrupalEasy Repositories Settings page and confirm we
+    // can reach it.
+    $this->drupalGet('/admin/config/services/repositories');
+    // Try this with a 500 status code to see it fail.
+    $session->statusCodeEquals(200);
   }
 
 }
---
+++ web/modules/custom/drupaleasy_repositories/tests/src/Traits/RepositoryContentTypeTrait.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Drupal\Tests\drupaleasy_repositories\Traits;
+
+use Drupal\field\Entity\FieldConfig;
+use Drupal\field\Entity\FieldStorageConfig;
+use Drupal\node\Entity\NodeType;
+
+/**
+ * Provides a helper method for creating a repository content type with fields.
+ */
+trait RepositoryContentTypeTrait {
+
+  /**
+   * Creates a repository content type with fields.
+   */
+  protected function createRepositoryContentType() {
+    NodeType::create(['type' => 'repository', 'name' => 'Repository'])->save();
+
+    // Create Description field.
+    FieldStorageConfig::create([
+      'field_name' => 'field_description',
+      'type' => 'text_long',
+      'entity_type' => 'node',
+      'cardinality' => 1,
+    ])->save();
+    FieldConfig::create([
+      'field_name' => 'field_description',
+      'entity_type' => 'node',
+      'bundle' => 'repository',
+      'label' => 'Description',
+    ])->save();
+
+    // Create Hash field.
+    FieldStorageConfig::create([
+      'field_name' => 'field_hash',
+      'type' => 'string',
+      'entity_type' => 'node',
+      'cardinality' => 1,
+    ])->save();
+    FieldConfig::create([
+      'field_name' => 'field_hash',
+      'entity_type' => 'node',
+      'bundle' => 'repository',
+      'label' => 'Hash',
+    ])->save();
+
+    // Create Machine name field.
+    FieldStorageConfig::create([
+      'field_name' => 'field_machine_name',
+      'type' => 'string',
+      'entity_type' => 'node',
+      'cardinality' => 1,
+    ])->save();
+    FieldConfig::create([
+      'field_name' => 'field_machine_name',
+      'entity_type' => 'node',
+      'bundle' => 'repository',
+      'label' => 'Machine name',
+    ])->save();
+
+    // Create Number of open issues field.
+    FieldStorageConfig::create([
+      'field_name' => 'field_number_of_issues',
+      'type' => 'integer',
+      'entity_type' => 'node',
+      'cardinality' => 1,
+    ])->save();
+    FieldConfig::create([
+      'field_name' => 'field_number_of_issues',
+      'entity_type' => 'node',
+      'bundle' => 'repository',
+      'label' => 'Number of open issues',
+    ])->save();
+
+    // Create Source field.
+    FieldStorageConfig::create([
+      'field_name' => 'field_source',
+      'type' => 'string',
+      'entity_type' => 'node',
+      'cardinality' => 1,
+    ])->save();
+    FieldConfig::create([
+      'field_name' => 'field_source',
+      'entity_type' => 'node',
+      'bundle' => 'repository',
+      'label' => 'Source',
+    ])->save();
+
+    // Create URL field.
+    FieldStorageConfig::create([
+      'field_name' => 'field_url',
+      'type' => 'link',
+      'entity_type' => 'node',
+      'cardinality' => 1,
+    ])->save();
+    FieldConfig::create([
+      'field_name' => 'field_url',
+      'entity_type' => 'node',
+      'bundle' => 'repository',
+      'label' => 'URL',
+    ])->save();
+  }
+
+}
