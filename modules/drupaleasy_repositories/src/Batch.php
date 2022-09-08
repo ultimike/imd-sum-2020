bd2a8d7ae3066251db53a1c8bf7947bd2dcee83a
Michael Anello <michael@anelloconsulting.com>
2022-09-08 20:50:02 +0200

    Added Update repositories form and Batch API stuff. End of class 18.

--- modules/drupaleasy_repositories/src/Batch.php
+++ modules/drupaleasy_repositories/src/Batch.php
@@ -4,55 +4,98 @@ namespace Drupal\drupaleasy_repositories;
 
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Extension\ModuleExtensionList;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\drupaleasy_repositories\DrupaleasyRepositoriesService;
 
 /**
- * Service description.
+ * Batch service class to integrate with Batch API.
  */
 class Batch {
 
+  use StringTranslationTrait;
+
   /**
-   * The drupaleasy_repositories.service service.
+   * The DrupaleasyRepositories service.
    *
    * @var \Drupal\drupaleasy_repositories\DrupaleasyRepositoriesService
    */
-  protected $service;
+  protected DrupaleasyRepositoriesService $drupaleasyRepositoriesService;
 
   /**
    * The entity type manager.
    *
    * @var \Drupal\Core\Entity\EntityTypeManagerInterface
    */
-  protected $entityTypeManager;
+  protected EntityTypeManagerInterface $entityTypeManager;
 
   /**
    * The module extension list.
    *
    * @var \Drupal\Core\Extension\ModuleExtensionList
    */
-  protected $extensionListModule;
+  protected ModuleExtensionList $extensionListModule;
 
   /**
    * Constructs a Batch object.
    *
-   * @param \Drupal\drupaleasy_repositories\DrupaleasyRepositoriesService $service
-   *   The drupaleasy_repositories.service service.
+   * @param \Drupal\drupaleasy_repositories\DrupaleasyRepositoriesService $drupaleasy_repositories_service
+   *   The DrupaleasyRepositories service.
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
    *   The entity type manager.
    * @param \Drupal\Core\Extension\ModuleExtensionList $extension_list_module
    *   The module extension list.
    */
-  public function __construct(DrupaleasyRepositoriesService $service, EntityTypeManagerInterface $entity_type_manager, ModuleExtensionList $extension_list_module) {
-    $this->service = $service;
+  public function __construct(DrupaleasyRepositoriesService $drupaleasy_repositories_service, EntityTypeManagerInterface $entity_type_manager, ModuleExtensionList $extension_list_module) {
+    $this->drupaleasyRepositoriesService = $drupaleasy_repositories_service;
     $this->entityTypeManager = $entity_type_manager;
     $this->extensionListModule = $extension_list_module;
   }
 
   /**
-   * Method description.
+   * Updates all user repositories using the Batch API.
    */
-  public function doSomething() {
-    // @DCG place your code here.
+  public function updateAllUserRepositories(): void {
+    $operations = [];
+
+    // Get all active users.
+    $user_storage = $this->entityTypeManager->getStorage('user');
+    $query = $user_storage->getQuery();
+    $query->condition('status', '1');
+    $users = $query->accessCheck(FALSE)->execute();
+
+    // Create a Batch API item for each user.
+    foreach ($users as $uid => $user) {
+      $operations[] = ['drupaleasy_update_repositories_batch_operation', [$uid]];
+    }
+    $batch = [
+      'operations' => $operations,
+      'finished' => 'drupaleasy_update_all_repositories_finished',
+      'file' => $this->extensionListModule->getPath('drupaleasy_repositories') . '/drupaleasy_repositories.batch.inc',
+    ];
+
+    // Submit the batch for processing.
+    batch_set($batch);
+  }
+
+  /**
+   * Batch process callback from updating user repositories.
+   *
+   * @param int $uid
+   *   User ID to update.
+   * @param array|\ArrayAccess $context
+   *   Context for operations. We do not want to type hint this as an array or
+   *   an object as sometimes it is an array (when calling from a form) and
+   *   sometimes it is an object (when calling from Drush).
+   */
+  public function updateRepositoriesBatch(int $uid, array|\ArrayAccess &$context): void {
+    $user_storage = $this->entityTypeManager->getStorage('user');
+    $account = $user_storage->load($uid);
+    $this->drupaleasyRepositoriesService->updateRepositories($account);
+    $context['results'][] = $uid;
+    $context['results']['num']++;
+    $context['message'] = $this->t('Updating repositories belonging to "@username".',
+      ['@username' => $account->label()]
+    );
   }
 
 }
--- modules/drupaleasy_repositories/src/Form/UpdateRepositoriesForm.php
+++ modules/drupaleasy_repositories/src/Form/UpdateRepositoriesForm.php
@@ -7,6 +7,7 @@ use Drupal\Core\Form\FormStateInterface;
 use Drupal\drupaleasy_repositories\DrupaleasyRepositoriesService;
 use Drupal\drupaleasy_repositories\Batch;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides a DrupalEasy Repositories form.
@@ -14,31 +15,68 @@ use Drupal\Core\Entity\EntityTypeManagerInterface;
 class UpdateRepositoriesForm extends FormBase {
 
   /**
+   * The DrupalEasy repositories service.
+   *
+   * @var \Drupal\drupaleasy_repositories\DrupaleasyRepositoriesService
+   */
+  protected DrupaleasyRepositoriesService $repositoriesService;
+
+  /**
+   * The DrupalEasy repositories batch service.
+   *
+   * @var \Drupal\drupaleasy_repositories\Batch
+   */
+  protected Batch $batch;
+
+  /**
+   * The Entity type manager service.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected EntityTypeManagerInterface $entityTypeManager;
+
+  /**
    * {@inheritdoc}
    */
-  public function getFormId() {
+  public static function create(ContainerInterface $container): UpdateRepositoriesForm {
+    return new static(
+      $container->get('drupaleasy_repositories.service'),
+      $container->get('drupaleasy_repositories.batch'),
+      $container->get('entity_type.manager'),
+    );
+  }
+
+  /**
+   * Class constructor.
+   */
+  public function __construct(DrupaleasyRepositoriesService $drupaleasy_repositories_service, Batch $batch, EntityTypeManagerInterface $entity_type_manager) {
+    $this->repositoriesService = $drupaleasy_repositories_service;
+    $this->batch = $batch;
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId(): string {
     return 'drupaleasy_repositories_update_repositories';
   }
 
   /**
    * {@inheritdoc}
    */
-  public function buildForm(array $form, FormStateInterface $form_state) {
-
-    $form['message'] = [
-      '#type' => 'textarea',
-      '#title' => $this->t('Message'),
-      '#required' => TRUE,
+  public function buildForm(array $form, FormStateInterface $form_state): array {
+    $form['uid'] = [
+      '#type' => 'entity_autocomplete',
+      '#target_type' => 'user',
+      '#title' => $this->t('Username'),
+      '#description' => $this->t('Leave blank to update all repository nodes for all users.'),
+      '#required' => FALSE,
     ];
-
-    $form['actions'] = [
-      '#type' => 'actions',
-    ];
-    $form['actions']['submit'] = [
+    $form['submit'] = [
       '#type' => 'submit',
-      '#value' => $this->t('Send'),
+      '#value' => 'Go',
     ];
-
     return $form;
   }
 
@@ -46,8 +84,8 @@ class UpdateRepositoriesForm extends FormBase {
    * {@inheritdoc}
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
-    if (mb_strlen($form_state->getValue('message')) < 10) {
-      $form_state->setErrorByName('message', $this->t('Message should be at least 10 characters.'));
+    if (!is_null($form_state->getValue('uid')) && ($form_state->getValue('uid') == 0)) {
+      $form_state->setErrorByName('uid', $this->t('You may not select the Anonymous user.'));
     }
   }
 
@@ -55,8 +93,24 @@ class UpdateRepositoriesForm extends FormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $this->messenger()->addStatus($this->t('The message has been sent.'));
-    $form_state->setRedirect('<front>');
+    if ($uid = $form_state->getValue('uid')) {
+      /** @var \Drupal\user\UserStorageInterface $user_storage */
+      $user_storage = $this->entityTypeManager->getStorage('user');
+
+      $account = $user_storage->load($uid);
+      if ($account) {
+        if ($this->repositoriesService->updateRepositories($account)) {
+          $this->messenger()->addMessage($this->t('Repositories updated.'));
+        }
+      }
+      else {
+        $this->messenger()->addMessage($this->t('User does not exist.'));
+      }
+    }
+    else {
+      $this->batch->updateAllUserRepositories();
+    }
+
   }
 
 }
